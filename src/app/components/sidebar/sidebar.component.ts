b1d6f469376e5f130f28d2a98bc56ed474620709
NisaanthK <117198338+NisaanthK@users.noreply.github.com>
2025-07-09 16:28:10 +0200

    Created folder and added sidebar

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-sidebar',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+  isCollapsed = false;
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
+// This component represents the sidebar of the application.
+// It includes a toggle button to collapse or expand the sidebar.
