568a9454a0044e7a1e0a7b64def5b17395b6358f
NisaanthK <117198338+NisaanthK@users.noreply.github.com>
2025-07-10 22:12:19 +0200

    "feat(pages):add-new-pages"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { DocsComponent } from './pages/docs/docs.component';
+import { ImagesComponent } from './pages/images/images.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: '**', redirectTo: '' }
+  { path: 'chat', component: ChatComponent },
+  { path: 'docs', component: DocsComponent },
+  { path: 'images', component: ImagesComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '**', redirectTo: '' },
 ];
