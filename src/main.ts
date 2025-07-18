8df5e78dc6d8ce8067bad1cd0353eb4f69cbd11f
NisaanthK <117198338+NisaanthK@users.noreply.github.com>
2025-07-18 17:43:14 +0200

    Updated css for promptbar

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouterModule }      from '@angular/router';
-import { HttpClientModule }  from '@angular/common/http';
-import { AppComponent }      from './app/app.component';
-import { routes }            from './app/app.routes';
+import { RouterModule } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
-      RouterModule.forRoot(routes),
-      HttpClientModule
-    )
+      RouterModule.forRoot(routes)
+    ),
+    provideHttpClient(withFetch())
   ]
 }).catch(err => console.error(err));
